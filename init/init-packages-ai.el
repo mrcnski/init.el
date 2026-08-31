0d041350e0eceffa949851d3bff8d2c7ec67e4a7
Marcin S. <marcin@realemail.net>
2026-08-31 18:40:30 -0500

    chore: remove copilot (wasn't used)

--- init/init-packages-ai.el
+++ init/init-packages-ai.el
@@ -109,47 +109,6 @@ alphabetically."
     )
   )
 
-;; REMOVED: Doesn't work.
-;; First-time setup:
-;; M-x copilot-install-server
-;; M-x copilot-login
-;;
-;; Check status:
-;; M-x copilot-diagnose
-;;
-;; See also https://robert.kra.hn/posts/2023-02-22-copilot-emacs-setup/.
-;; (use-package copilot
-;;   :vc (:url "https://github.com/copilot-emacs/copilot.el"
-;;             :rev :newest
-;;             :branch "main")
-;;  :hook ((prog-mode conf-mode) . copilot-mode)
-;;  :bind (
-;;         ("s-/" . copilot-complete)
-
-;;         :map copilot-completion-map
-;;         ("<tab>" . copilot-accept-completion)
-;;         ("TAB"   . copilot-accept-completion)
-;;         ;; ("C-f" . copilot-accept-completion)
-;;         ("M-f" . copilot-accept-completion-by-word)
-;;         ("C-e" . copilot-accept-completion-by-line)
-;;         ("s-/" . copilot-accept-completion)
-;;         ("C-g" . copilot-clear-overlay)
-;;         ("M-n" . copilot-next-completion)
-;;         ("M-p" . copilot-previous-completion)
-;;         )
-;;  :config
-;;  (setq
-;;   ;; Disable the idle delay?
-;;   copilot-idle-delay nil
-;;   copilot-indent-offset-warning-disable t
-;;   ;; Try to reduce the warning rate.
-;;   ;;
-;;   ;; NOTE: Try setting `copilot-max-char-warning-disable' if this continues to
-;;   ;; be a problem.
-;;   copilot-max-char (* 1000 1000)
-;;   )
-;;  )
-
 (use-package dream-search
   :load-path "~/.emacs.d/packages/dream-search"
   ;; Needed for load-path packages.
